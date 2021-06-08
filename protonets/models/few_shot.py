cae3ac4abe461017bcb895fe6efe9e5ecc462165
Littleor <admin@sixming.com>
2021-06-08 20:46:51 +0800

    深刻理解模型的输入输出的含义和损失函数的计算

--- protonets/models/few_shot.py
+++ protonets/models/few_shot.py
@@ -12,6 +12,7 @@ class Flatten(nn.Module):
         super(Flatten, self).__init__()
 
     def forward(self, x):
+        # 展开为二维张量
         return x.view(x.size(0), -1)
 
 
@@ -23,36 +24,66 @@ class Protonet(nn.Module):
 
     # 计算错误率
     def loss(self, sample):
+        # print("sample", sample['xs'].size())
+        # torch.Size([60, 5, 1, 28, 28])
+        # 意思是60类 每类5个样本 每个样本为28 * 28的图
         xs = Variable(sample['xs'])  # support
         xq = Variable(sample['xq'])  # query
 
-        n_class = xs.size(0)
-        assert xq.size(0) == n_class
-        n_support = xs.size(1)
-        n_query = xq.size(1)
+        n_class = xs.size(0)  # N类 -> 60
+        assert xq.size(0) == n_class  # Support Set 和 Query Set的类总数需要保持一致
+        n_support = xs.size(1)  # 多少个Support Example -> 5
+        n_query = xq.size(1)  # 多少个Query Example -> 5
 
+        # 每个example的label，也就是对应的分类
+        # torch.arange(0, n_class) : [0, 1, 2, 3, 4, ... ,n_class - 1] -> Size([n_class])
+        # torch.view(n_class, 1, 1): [ [[0]], [[1]], [[2]], ..., [[n_class - 1]]] -> Size([n_class, 1, 1])
+        # torch.expand(n_class, n_query, 1): [ [[0] * n_query], [[1] * n_query], ..., [[n_class - 1] * n_query]]
+        #   不扩展内存只是指针调用来扩展张量
+        # 因为我们是按顺序来的[60, 5, 1, 28, 28] -> 60类 每类5个example  每个example是[1, 28, 28]的图
+        # 打标的时候应该是每个图对应一种，我们直接按index打标签，也就是第一类的为0，第二类为1...
+        # 即输出应该为 [60, 5, 60] 每个图输出60个数字代表每种类型的概率，取最大就是我们的预测值
         target_inds = torch.arange(0, n_class).view(n_class, 1, 1).expand(n_class, n_query, 1).long()
-        target_inds = Variable(target_inds, requires_grad=False)
+        # torch.Size([60, 5, 1]) 代表60类 每类5个 每个代表同一个类别
+        target_inds = Variable(target_inds, requires_grad=False)  # 不计算梯度
 
+        # 显卡判断
         if xq.is_cuda:
             target_inds = target_inds.cuda()
 
+        # 将Support Set 和 Query Set中的图片直接扩展出来
         x = torch.cat([xs.view(n_class * n_support, *xs.size()[2:]),
-                       xq.view(n_class * n_query, *xq.size()[2:])], 0)
+                       xq.view(n_class * n_query, *xq.size()[2:])], 0)  # 安装维度0来拼接
+        # print(x.size()) -> torch.Size([600, 1, 28, 28])
 
+        # 前向传输 也就是进入网络处理
+        # 新版本中无需调用forward，实例化的时候就会自动去调用
         z = self.encoder.forward(x)
-        z_dim = z.size(-1)
+        # print('z', z.size()) -> torch.Size([600, 64])
 
-        z_proto = z[:n_class * n_support].view(n_class, n_support, z_dim).mean(1)
-        zq = z[n_class * n_support:]
+        z_dim = z.size(-1)  # 64
+        # n_class * n_support = 60 * 5 = 300
+        # z[:n_class * n_support].view(n_class, n_support, z_dim): Size([60, 5, 64])
+        # z_proto: Size([60, 64])
+        # 下面的z_proto就是原型的位置！！！也就是对矢量取平均得到的中心位置！
+        z_proto = z[:n_class * n_support].view(n_class, n_support, z_dim).mean(1)  # 在轴为1上进行取平均 Size([60, 64])
+        # 下面是Query Set处理后的数据
+        zq = z[n_class * n_support:]  # Query Set处理后的 Size([300, 64])  -> 300 个Example 每个为64
 
-        dists = euclidean_dist(zq, z_proto)
+        # 求出欧式距离
+        dists = euclidean_dist(zq, z_proto)  # Size([300, 60])
 
-        log_p_y = F.log_softmax(-dists, dim=1).view(n_class, n_query, -1)
+        # log(softmax(x))
+        # F.log_softmax(-dists, dim=1).size() -> Size([300, 60])
+        log_p_y = F.log_softmax(-dists, dim=1).view(n_class, n_query, -1)  # Size([60, 5 , 60])
+        # log_p_y的意思是有60类 每类5张 每张对应的60种的概率
 
+        # target_inds -> Size([60, 5, 1])
+        # -log_p_y.gather(2, target_inds) -> Size([60 , 5, 1])
         loss_val = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
-
+        # 在2轴上取最大值的索引y_hat，也就是属于哪一类
         _, y_hat = log_p_y.max(2)
+        # 然后和target_inds比较，如果相等就是预测对了
         acc_val = torch.eq(y_hat, target_inds.squeeze()).float().mean()
 
         return loss_val, {
@@ -81,7 +112,7 @@ def load_protonet_conv(**kwargs):
         conv_block(hid_dim, hid_dim),
         conv_block(hid_dim, hid_dim),
         conv_block(hid_dim, z_dim),
-        Flatten()
+        Flatten()  # 展开为二维张量
     )  # 通过Sequential把前向传输过程给省略了
     # encoder在这里就是这个网络的结构 它用了非一般写法来适应动态参数问题 可以改写成一般写法
     return Protonet(encoder)
--- protonets/models/utils.py
+++ protonets/models/utils.py
@@ -1,15 +1,18 @@
 import torch
 
 
+# 求x和y的欧氏距离
 def euclidean_dist(x, y):
-    # x: N x D
-    # y: M x D
+    # x: N x D [300 x 64] -> 多个点
+    # y: M x D [60 x 64]  -> 原型
     n = x.size(0)
     m = y.size(0)
     d = x.size(1)
     assert d == y.size(1)
-
-    x = x.unsqueeze(1).expand(n, m, d)
-    y = y.unsqueeze(0).expand(n, m, d)
-
-    return torch.pow(x - y, 2).sum(2)
+    # x.unsqueeze(1): Size([N, 1, D])   [300, 1, 64] -> [300, 60, 64] -> 一维 -> 二维
+    # y.unsqueeze(0): Size([1, M, D])   [1, 60, 64] -> [300, 60, 64] -> 化成300个点
+    #
+    x = x.unsqueeze(1).expand(n, m, d)  # Size([N, M, D])
+    y = y.unsqueeze(0).expand(n, m, d)  # Size([N, M, D])
+    # 求欧式距离
+    return torch.pow(x - y, 2).sum(2)   # Size([N, M])
